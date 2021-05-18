96c45afecea912e681025958fec85648ae2658c9
Zuzanna Usakiewicz <z.usakiewicz@gmail.com>
2021-05-18 14:55:43 +0200

    create async function to fetch data from external api

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
 import "./style.css";
 
-document.querySelector("#app").innerHTML = `
-  <h1>Hello Vite!</h1>
+async function getQuestions() {
+  const url =
+    "https://opentdb.com/api.php?amount=10&category=10&difficulty=medium&type=multiple";
+  const response = await fetch(url);
+  const data = await response.json();
+  const incorrectQ = [...data.results[0].incorrect_answers];
 
-`;
+  document.getElementById(
+    "app"
+  ).innerHTML = `<h1>${data.results[0].question}</h1>
+  `;
+  // incorrectQ.forEach((item) => {
+  //     document.createElement("p").textContent = item;
+  //   });
+}
+getQuestions();
